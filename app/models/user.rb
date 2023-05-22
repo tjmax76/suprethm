c8c786a88a4c401664d19d5fa10e9bfaa71a62cb
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-05-22 22:18:31 +0900

    Userモデルにアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Base::Associations::ActiveRecordExtensions
+  belongs_to :chrono_type
+  
 end
