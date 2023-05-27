ccf8c29ce1c18ae267e8f173ddd804457cd032e7
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-05-27 23:43:21 +0900

    total関数の内容を記述

--- app/javascript/judge_chrono.js
+++ app/javascript/judge_chrono.js
@@ -1,16 +1,48 @@
-
-
 // total関数でラジオボタンの値の取得と合計を行う
 const total = function(){
-  q1 = document.getElementsByName('q1');
-  q1_len = q1.length;
-  
-    for(i = 0; i < q1_len; i++ ){
-      q1[i].addEventListener('click', () =>
-      {
-        console.log(q1[i] + 'is checked');
-      });
+  const submit = document.getElementById('submit');
+  submit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const answers = [];
+    const questions = [];
+
+    for(i = 1; i < 11; i++){
+      questions.push('q' + i);
     }
+
+    
+    const qList = [];
+    questions.forEach((q) => {
+      qList.push(document.getElementsByName(q));
+    });
+    
+    qList.forEach((q) => {
+      for(i = 0; i < q.length; i++){
+        if(q[i].checked){
+          answers.push(q[i].value);
+        }
+      }
+    });
+
+    const scores = [];
+    answers.forEach((e) => {
+      if(e == "yes"){
+        scores.push(1);
+      } else if(e == "neither"){
+        scores.push(2);
+      } else if(e == "no"){
+        scores.push(3);
+      }
+    });
+
+    totalPoints = 0;
+    for(i = 0; i < scores.length; i++){
+      totalPoints += scores[i];
+    }
+
+    console.log(totalPoints);
+  });
 };
 
 window.addEventListener('load', total);
