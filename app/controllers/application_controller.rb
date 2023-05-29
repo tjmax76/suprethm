358408f42ecd063866f65380d663cacd27d70a34
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-05-29 23:02:11 +0900

    ストロングパラメーターを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :chrono])
   end
 
   def set_user
