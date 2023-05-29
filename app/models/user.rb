546001aa824c155be40a2c1603e17b0537e20324
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-05-29 23:02:37 +0900

    アソシエーションを変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :chrono_type
+  belongs_to :chrono
 
 end
