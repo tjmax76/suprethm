22531ff2fd88b686176f8fc924b0c867ecca6f8d
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-05-31 22:58:47 +0900

    createアクションの中身を記述

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,5 +3,12 @@ class SchedulesController < ApplicationController
   end
 
   def create
+    schedule = Schedule.create(schedule_params)
+    schedules = Schedule.where(user_id: current_user.id).order(started_at: "ASC")
+    render json: { schedules: schedules }
+  end
+
+  def schedule_params
+    params.permit(:event_name, :started_at, :finished_at, :user_id)
   end
 end
