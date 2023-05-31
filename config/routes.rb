8c66d5260b9c3523e9faee5e756ef4832f4e8df6
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-05-31 14:38:20 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  
-  get '/users/:chrono_id/sign_up', to: 'devise/registrations#new', as: :new_user_signup
   devise_for :users
+  devise_scope :user do
+    get '/:chrono_id/sign_up', to: 'devise/registrations#new'
+    post '/:chrono_id/sign_up', to: 'devise/registrations#create'
+  end
+
   root to: "homes#index"
   resources :users, only: :show
   resources :chronos, only: [:index, :create, :new]
