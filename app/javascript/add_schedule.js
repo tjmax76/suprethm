2063edd9c33cdc57f8fb54e2368e58f39675f67b
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-06-02 07:53:17 +0900

    add_shedule.jsの処理が実行されるURLを制限

--- app/javascript/add_schedule.js
+++ app/javascript/add_schedule.js
@@ -32,38 +32,39 @@ const buildHTML = (events) => {
 };
 
 const add = function() {
-  const submit = document.getElementById('event-submit');
-  submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    const currentUrl = window.location.href;
-    const url = currentUrl.replace(/\/new/, "");
-    const form = document.getElementById('schedule-form');
-    const formData = new FormData(form);
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.responseType = "json";
-    xhr.send(formData);
-
-    xhr.onload = () => {
-      const events = xhr.response.schedules;
-      const list = document.getElementById('schedule-list');
-      list.innerHTML = "";
-      html = buildHTML(events);
-      html.forEach(element => {
-        element += "<br>";
-        list.insertAdjacentHTML('afterbegin', element);
-      });
-      const formValue1 = document.getElementById('event_name');
-      const formValue2 = document.getElementById('started_at');
-      const formValue3 = document.getElementById('finished_at');
-
-      formValue1.value = "";
-      formValue2.value = "";
-      formValue3.value = "";
-      const selectWeek = document.getElementById('select-week');
-      selectWeek.style.display = "none";
-    };
-  });
-};
+  const currentUrl = window.location.href;
+  if(currentUrl.match(/schedules\/new/)){
+    const submit = document.getElementById('event-submit');
+    submit.addEventListener('click', (e) => {
+      e.preventDefault();
+      const url = currentUrl.replace(/\/new/, "");
+      const form = document.getElementById('schedule-form');
+      const formData = new FormData(form);
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", url, true);
+      xhr.responseType = "json";
+      xhr.send(formData);
+      
+      xhr.onload = () => {
+        const events = xhr.response.schedules;
+        const list = document.getElementById('schedule-list');
+        list.innerHTML = "";
+        html = buildHTML(events);
+        html.forEach(element => {
+          element += "<br>";
+          list.insertAdjacentHTML('afterbegin', element);
+        });
+        const formValue1 = document.getElementById('event_name');
+        const formValue2 = document.getElementById('started_at');
+        const formValue3 = document.getElementById('finished_at');
+        
+        formValue1.value = "";
+        formValue2.value = "";
+        formValue3.value = "";
+        const selectWeek = document.getElementById('select-week');
+        selectWeek.style.display = "none";
+      };
+    });
+}};
 
 window.addEventListener('load', add);
