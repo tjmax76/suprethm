bf67a1bf949329614db617b3990b3f1c560bb685
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-06-02 07:16:03 +0900

    weekモデルとのアソシエーションを記述

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -5,5 +5,10 @@ class Schedule < ApplicationRecord
     validates :finished_at
   end
 
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :week_id, numericality: { other_than: 1, message: "平日もしくは休日を選択してください" }
+  
+  belongs_to :week
   belongs_to :user
 end
