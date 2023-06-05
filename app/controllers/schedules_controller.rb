821362fb5c8aba59b67fab9f63458ef3ce755ec9
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-06-05 21:08:00 +0900

    各アクションの編集

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -9,15 +9,27 @@ class SchedulesController < ApplicationController
   end
   
   def new
+    @schedule = Schedule.new
+    @user = User.find(current_user.id)
   end
 
   def create
-    schedule = Schedule.create(schedule_params)
+    @schedule = Schedule.create(schedule_params)
+    @user = User.find(current_user.id)
     schedules = Schedule.where(user_id: current_user.id).order(started_at: "DESC")
     render json: { schedules: schedules }
   end
 
+  def destroy
+    schedule = Schedule.find(params[:id])
+    week = Week.find(schedule.week_id)
+    user = User.find(schedule.user_id)
+    schedule.destroy
+    schedules = Schedule.where(user_id: current_user.id).order(started_at: "DESC")
+    render json: { schedules: schedules, user: user, week: week }
+  end
+
   def schedule_params
-    params.permit(:event_name, :started_at, :finished_at, :user_id, :week_id)
+    params.require(:schedule).permit(:event_name, :started_at, :finished_at, :week_id).merge(user_id: current_user.id)
   end
 end
