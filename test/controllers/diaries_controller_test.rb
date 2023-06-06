1be08e42f48a07228edbe885de82ba947e996882
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-06-07 06:29:13 +0900

    diariesコントローラーを生成

---
+++ test/controllers/diaries_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class DiariesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get diaries_index_url
+    assert_response :success
+  end
+
+  test "should post new" do
+    post diaries_post_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get diaries_show_url
+    assert_response :success
+  end
+
+end
